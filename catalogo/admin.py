90a179bd8e6e0604fb70ca1e98059fad870c3414
Bruno Alejandro <brunoalejandrodev@gmail.com>
2025-06-15 16:54:57 -0300

    feat update no arquivo models

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,8 +1,1 @@
 from django.contrib import admin
-from .models import Produto
-
-@admin.register(Produto)
-class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'valor', 'criado_em', 'atualizado_em')
-    search_fields = ('nome', 'descricao')
-    list_filter = ('criado_em', 'atualizado_em')
