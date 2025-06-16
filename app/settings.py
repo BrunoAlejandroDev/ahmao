a16593c162c359933aabf168612ea87171297d1f
Bruno Alejandro <brunoalejandrodev@gmail.com>
2025-06-16 19:00:11 -0300

    fix erro na definição de configurações do projeto

--- app/settings.py
+++ app/settings.py
@@ -19,6 +19,8 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = config('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
