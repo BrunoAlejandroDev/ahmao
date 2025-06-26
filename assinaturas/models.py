485f332ed47e48611ca24e32dabf88d6ac877af8
Bruno Alejandro <brunoalejandrodev@gmail.com>
2025-06-25 21:48:49 -0300

    feat: nova coluna no modelo de assinatura

--- assinaturas/models.py
+++ assinaturas/models.py
@@ -8,6 +8,7 @@ class Plan(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     frequency = models.IntegerField() # Ex: 30 para mensal
+    is_activate = models.BooleanField(default=True)
     def __str__(self): return self.name
 
 class Subscription(models.Model):
